ac5d34bdc62b0b6c8e64f6f782fff18ef57fea47
carlos monterrosa <carlosmontess07@gmail.com>
2021-01-29 01:05:07 -0500

    fix of fuit block

--- conexion.php
+++
@@ -1,9 +0,0 @@
-<?php
-$serverName = "localhost\SQLEXPRESS";
-$connectionInfo = array("Database"=>"itch");
-$conn = sqlsrv_connect($serverName, $connectionInfo);
-if($conn===false){
-    echo "conexión no establecida.<br/ >";
-    die(print_r( srlsrv_errors(), true));
-}
-?>
