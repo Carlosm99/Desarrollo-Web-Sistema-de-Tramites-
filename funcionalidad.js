4283691a6cff605e5ac126cbdec58ae023542d6f
carlos monterrosa <carlosmontess07@gmail.com>
2021-02-04 00:31:04 -0500

    adding calories of products

--- funcionalidad.js
+++ funcionalidad.js
@@ -1,4 +1,30 @@
-function box1 ()
-{
-    alert("I am an alert box!");
-}
+const charactersList = document.getElementById('charactersList');
+let alldata = [];
+
+const loadCharacters = async () => {
+    try {
+        var myRequest = new Request('fruits.json');          
+        const res = await fetch(myRequest);
+        alldata = await res.json();
+        displayCharacters(alldata);
+    } catch (err) {
+        console.error(err);
+    }
+};
+
+const displayCharacters = (fruits) => {
+    const htmlString = fruits
+        .map((fruit) => {
+            return `
+            <li class="character">
+                <h2>${fruit.name}</h2>                
+                <img src="${fruit.image}"></img>
+            </li>
+        `;
+        })
+        .join('');
+    charactersList.innerHTML = htmlString;
+};
+
+loadCharacters();
+
