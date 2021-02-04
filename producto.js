50943405f4be49288af3f8593b06a33bbce8b9d6
FranceCawich <L17390321@chetumal.tecnm.mx>
2021-02-04 02:17:58 -0600

    modificacion de alert box atraves de una funcion y liberias externas

---
+++ producto.js
@@ -0,0 +1,11 @@
+
+function box1(){
+
+
+    Swal.fire({
+        title: 'exitosa!',
+        text: 'se agrego exitosament',
+        icon: 'success',
+        confirmButtonText: 'listo'
+      })
+}
